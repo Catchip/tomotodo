a67c91064826052349a9251a3dd6d0253d0d6484
Catchip <catchip@qq.com>
2020-02-28 17:11:23 +0800

    Add a new Activity for showing learning data,delete some compents

--- app/src/main/java/com/example/tomotodo/Showdata_Activity.java
+++ app/src/main/java/com/example/tomotodo/Showdata_Activity.java
@@ -3,18 +3,12 @@ package com.example.tomotodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Showdata_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void sendMessage(View view){
-        // DO something in response to button
-
+        setContentView(R.layout.activity_showdata_);
     }
 }
