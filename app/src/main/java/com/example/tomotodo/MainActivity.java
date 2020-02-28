6ff79ceca8c6a2ea6d0dde8c4974ae1f6e2e6899
Catchip <catchip@qq.com>
2020-02-28 21:11:48 +0800

    switching between those four acticities

--- app/src/main/java/com/example/tomotodo/MainActivity.java
+++ app/src/main/java/com/example/tomotodo/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.tomotodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +15,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
-        // DO something in response to button
-
+    public void switchfuction(View view){
+        Integer i = view.getId();
+        switch (view.getId()){
+            case R.id.button1:
+                Intent intent0 = new Intent(this,MainActivity.class);
+                startActivity(intent0);
+                break;
+            case R.id.button2:
+                Intent intent1 = new Intent(this,Showdata_Activity.class);
+                startActivity(intent1);
+                break;
+            case R.id.button3:
+                Intent intent2 = new Intent(this,Lock.class);
+                startActivity(intent2);
+                break;
+            case R.id.button4:
+                Intent intent3 = new Intent(this,Mine.class);
+                startActivity(intent3);
+                break;
+            default:
+                Button button = findViewById(R.id.button6);
+                button.setText("assad");
+        }
     }
 }
--- app/src/main/java/com/example/tomotodo/Showdata_Activity.java
+++ app/src/main/java/com/example/tomotodo/Showdata_Activity.java
@@ -2,7 +2,10 @@ package com.example.tomotodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Showdata_Activity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class Showdata_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showdata_);
+    }
+
+    public void switchfuction(View view){
+        Integer i = view.getId();
+        switch (view.getId()){
+            case R.id.button1:
+                Intent intent0 = new Intent(this,MainActivity.class);
+                startActivity(intent0);
+                break;
+            case R.id.button2:
+                Intent intent1 = new Intent(this,Showdata_Activity.class);
+                startActivity(intent1);
+                break;
+            case R.id.button3:
+                Intent intent2 = new Intent(this,Lock.class);
+                startActivity(intent2);
+                break;
+            case R.id.button4:
+                Intent intent3 = new Intent(this,Mine.class);
+                startActivity(intent3);
+                break;
+            default:
+                Button button = findViewById(R.id.button6);
+                button.setText("assad");
+        }
     }
 }
